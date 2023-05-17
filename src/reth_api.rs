59df55949ba5206a438bd9c5a08f8eccfb4d4627
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-05-17 14:15:15 +0200

    fmt

--- src/reth_api.rs
+++ src/reth_api.rs
@@ -1,4 +1,4 @@
-
+use crate::defaults::{default_client, default_pool, NodeClient, NodeTxPool};
 use async_trait::async_trait;
 use ethers::prelude::Client;
 use reth_beacon_consensus::BeaconConsensus;
@@ -11,33 +11,30 @@ use reth_primitives::MAINNET;
 use reth_provider::{providers::BlockchainProvider, ShareableDatabase};
 use reth_revm::Factory;
 use reth_rpc::{
-    eth::{cache::{EthStateCache, EthStateCacheConfig}, gas_oracle::{GasPriceOracle, GasPriceOracleConfig}},
+    eth::{
+        cache::{EthStateCache, EthStateCacheConfig},
+        gas_oracle::{GasPriceOracle, GasPriceOracleConfig},
+    },
     EthApi,
 };
-use reth_transaction_pool::{EthTransactionValidator, Pool, PooledTransaction, CostOrdering};
-use std::{sync::Arc, path::Path};
-use crate::{defaults::{default_pool, default_client, NodeClient, NodeTxPool}};
-
+use reth_transaction_pool::{CostOrdering, EthTransactionValidator, Pool, PooledTransaction};
+use std::{path::Path, sync::Arc};
 
 pub type NodeEthApi = EthApi<NodeClient, NodeTxPool, NoopNetwork>;
 
-
 /// default EthApi implementation
 pub fn default_eth_api(client: NodeClient) -> NodeEthApi {
-
     let tx_pool = default_pool(client.clone());
 
     let state_cache = EthStateCache::spawn(client.clone(), EthStateCacheConfig::default());
 
     let api = EthApi::new(
-        client.clone(), 
-        tx_pool, 
-        NoopNetwork, 
-        state_cache, 
-        GasPriceOracle::new(client.clone(), GasPriceOracleConfig::default(), state_cache.clone())
+        client.clone(),
+        tx_pool,
+        NoopNetwork,
+        state_cache,
+        GasPriceOracle::new(client.clone(), GasPriceOracleConfig::default(), state_cache.clone()),
     );
-    
+
     api
 }
-
-
