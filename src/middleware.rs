e98e0cc29e6628e36f2fc0a1234aeaa3623b97d1
joenoor <lagrange@sorellacapital.com>
2023-06-06 20:05:05 +0200

    updated tests

--- src/middleware.rs
+++ src/middleware.rs
@@ -409,9 +409,49 @@ mod tests {
         let address = middleware.get_address(ens).await.unwrap();
         assert_eq!(address, "0x0e3FfF21A1Cef4f29F7D8cecff3cE4Dfa7703fBc".parse().unwrap());
 
-        rt.shutdown_timeout(Duration::from_secs(0));
+            rt.shutdown_timeout(Duration::from_secs(0));
     }
 
+    #[tokio::test]
+    async fn test_get_storage_at() {
+        // Create a runtime and handle here for the TaskManager
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let handle = rt.handle();
+
+        let provider = spawn_http_provider(TEST_HTTP_URL).await;
+        let middleware = RethMiddleware::new(provider, Path::new(TEST_DB_PATH), handle);
+
+        let from: NameOrAddress = "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852".parse().unwrap();
+        let location = EthersH256::from_low_u64_be(5);
+        let storage = middleware.get_storage_at(from, location, None).await.unwrap();
+
+        // ----------------------------------------------------------- //
+        //             Storage slots of UniV2Pair contract             //
+        // =========================================================== //
+        // storage[5] = factory: address                               //
+        // storage[6] = token0: address                                //
+        // storage[7] = token1: address                                //
+        // storage[8] = (res0, res1, ts): (uint112, uint112, uint32)   //
+        // storage[9] = price0CumulativeLast: uint256                  //
+        // storage[10] = price1CumulativeLast: uint256                 //
+        // storage[11] = kLast: uint256                                //
+        // =========================================================== //
+
+        // convert the H256 value to bytes, then take the last 20 bytes and create an address from
+        // it
+        let decoded_addr = EthersAddress::from_slice(&storage.as_bytes()[12..]);
+
+        let factory_address: NameOrAddress =
+            "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f".parse().unwrap();
+
+        if let NameOrAddress::Address(expected_addr) = factory_address {
+            assert_eq!(decoded_addr, expected_addr);
+        } else {
+            panic!("Failed to parse expected factory address");
+        }
+
+        rt.shutdown_timeout(Duration::from_secs(0));
+    }
 
     #[tokio::test]
     async fn test_get_code() {
